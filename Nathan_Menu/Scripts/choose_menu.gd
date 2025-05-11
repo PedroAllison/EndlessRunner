8cd8afb1423f6251b83b3376812673d2c761e713
N3YtoIn <nathan.dev2004@gmail.com>
2025-05-11 20:41:29 -0300

    feat: add menu

---
+++ Nathan_Menu/Scripts/choose_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_versus_pressed():
+	get_tree().change_scene_to_file("res://Scenes/.tscn")
+	pass # Replace with function body.
+
+func _on_survive_pressed():
+	get_tree().change_scene_to_file("res://Scenes/.tscn")
+	pass # Replace with function body.
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	pass # Replace with function body.
