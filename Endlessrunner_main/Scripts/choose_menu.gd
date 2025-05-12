78da4bc5cacbd698156f9434d222747f6dbf88ba
N3YtoIn <nathan.dev2004@gmail.com>
2025-05-11 21:06:44 -0300

    feat: integrated menu to modes

--- Endlessrunner_main/Scripts/choose_menu.gd
+++ Endlessrunner_main/Scripts/choose_menu.gd
@@ -1,11 +1,11 @@
 extends Control
 
 func _on_versus_pressed():
-	get_tree().change_scene_to_file("res://Scenes/.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Versus.tscn")
 	pass # Replace with function body.
 
 func _on_survive_pressed():
-	get_tree().change_scene_to_file("res://Scenes/.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Survive.tscn")
 	pass # Replace with function body.
 
 func _on_back_pressed():
